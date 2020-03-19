9663978d2d549825a7705aa7378b0d59612e69c4
tranhuyitqnu <harukikb@gmail.com>
2020-03-19 13:51:16 +0700

    huy push giao dien

---
+++ resources/views/Admin/Share/sidebar.blade.php
@@ -0,0 +1,59 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset ('admin/dist/img/user2-160x160.jpg ') }}" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+                <p>Admin</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">MAIN NAVIGATION</li>
+            <li><a href="{{ url ('admin/subject') }}"><i class="fa fa-book"></i> <span>Môn Học</span></a></li>
+            <li><a href="{{ url ('admin/student') }}"><i class="fa fa-group"></i> <span>Học Sinh</span></a></li>
+            <li><a href="{{ asset ('admin/question') }}"><i class="fa fa-question-circle"></i> Câu hỏi</a></li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-file"></i>
+                    <span>Thi</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="{{ asset ('admin/exam') }}"><i class="fa fa-circle-o"></i> Danh sách đề thi</a></li>
+                    <li><a href="{{ asset ('admin/listexam') }}"><i class="fa fa-circle-o"></i> Danh sách thi</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-pencil-square-o"></i>
+                    <span>Kết Quả</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="{{ asset ('admin/task') }}"><i class="fa fa-circle-o"></i> Bài làm</a></li>
+                    <li><a href="{{ asset ('admin/result') }}"><i class="fa fa-circle-o"></i> Kết quả bài</a></li>
+                </ul>
+            </li>
+    </section>
+    <!-- /.sidebar -->
+</aside>
---
+++ resources/views/Admin/Student/student.blade.php
@@ -0,0 +1,91 @@
+@extends('admin.share.master')
+@section('title','Học sinh')
+@section('content')
+<section class="content-header">
+    <h1>
+        Học Sinh
+        <small></small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Học Sinh</a></li>
+        <li class="active">Danh sách học sinh</li>
+    </ol>
+</section>
+
+<section class="content">
+    <div id="error_message">
+
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-primary">
+                <div class="box-header" style="text-align: height: 50px;line-height: 50px;white-space: nowrap;">
+                    <h3 class="box-title">Danh sách học sinh</h3>
+                    <div class="pull-right">
+                        <div class="col-xs-9">
+                            <form  method="post" enctype="multipart/form-data" id="file_form"> 
+                                <a class="btn btn-app ">
+                                    <input type="file" id="inputFile" name="inputFile">
+                                </a>
+                                <a id ="btn_import" class="btn btn-app btn_import"><i class="glyphicon glyphicon-import"></i>Import</a>
+                            </form>
+                        </div>
+                        <div class="col-xs-3">
+                            <a href="{{url('export_excel')}}" class="btn btn-app "><i class="glyphicon glyphicon-export"></i>Export</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box-body">
+                    <table id="example1" class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>Tên</th>
+                                <th>Ngày sinh</th>
+                                <th>Giới tính</th>
+                                <th>CMND</th>
+                                <th>Môn Thi</th>
+                                <th>Công cụ</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($student as $item )
+                                <tr>
+                                    <td>{{ $item->id}}</td>
+                                    <td>{{ $item->fullname}}</td>
+                                    <td>{{ $item->namsinh}}</td>
+                                    <td>
+                                        @if ($item['gender']==1) Nam
+                                        @else Nữ
+                                        @endif
+
+                                    </td>
+                                    <td>{{ $item->cmnd}}</td>
+                                    <td>{{ $item->list_subject}} </td>
+                                    <td>
+                                        <i title="Sửa" class="fa fa-pencil-square-o" style="margin-right: 5px;margin-left: 5px;"></i>
+
+                                        @if ($item['isActive']==1) <i title=" Hiện " class="fa fa-eye"> </i>
+                                        @else <i title=" Ẩn " class="fa fa-eye-slash"> </i>
+                                        @endif
+
+                                        <i title=" Xóa " class="fa fa-trash-o " style="color: darkred; "></i>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</section>
+<!-- /.content -->
+
+@endsection()

6189071aa08213e531ac1ba35109c222be1d3048
Hanh Phan <honghanhpretty20@gmail.com>
2020-03-19 14:56:39 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,40 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'Client\HomeController@index');
+//admin route
+Route::get('/admin', 'Auth\LoginController@index');
+
+Route::get('/admin/login', 'Auth\LoginController@getLogin');
+
+Route::post('/admin/login', 'Auth\LoginController@postLogin');
+
+Route::get('/admin/logout', 'Auth\LogoutController@getLogout');
+
+Route::get('/admin/home', 'Admin\HomeController@index');
+
+Route::get('/admin/question', 'Admin\ExamController@index');
+
+Route::get('/admin/exam', 'Admin\ExamController@index');
+
+Route::get('/admin/listexam', 'Admin\ExamController@listexam');
+
+Route::get('/admin/subject', 'Admin\SubjectController@index');
+
+Route::get('/admin/subject/addsub', 'Admin\SubjectController@add');
+
+Route::get('/subject', 'SubjectController@index');
+
+Route::get('/admin/student', 'Admin\StudentController@index');
+
+Route::post('/admin/student/importfile', 'Admin\StudentController@importfile');
+
+Route::get('/admin/student/addstu', 'Admin\StudentController@add');
+
+Route::get('/admin/question', 'Admin\QuestionController@index');
+
+Route::get('/admin/question/addque', 'Admin\QuestionController@add');
+
+Route::get('export_excel', 'Admin\ExportExcelController@export');
+
+Route::post('/admin/subject','Admin\SubjectController@doUpLoad');
